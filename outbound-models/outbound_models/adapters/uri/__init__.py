3ee63a30fa6cfd4d837fc6e2dc412dad21ac5352
awesfdawe <124027549+awesfdawe@users.noreply.github.com>
2026-05-31 00:34:02 +0200

    Remove netloc and user check from uri adapter

--- outbound-models/outbound_models/adapters/uri/__init__.py
+++ outbound-models/outbound_models/adapters/uri/__init__.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlsplit, parse_qs
 
-from outbound_models.outbounds.exceptions import UnsupportedProtocolError, InvalidURIError
+from outbound_models.outbounds.exceptions import UnsupportedProtocolError
 from outbound_models.outbounds import AnyOutbound
 from outbound_models.outbounds.vless import VlessOutbound
 from outbound_models.outbounds.hysteria2 import Hysteria2Outbound
@@ -8,10 +8,6 @@ from outbound_models.outbounds.hysteria2 import Hysteria2Outbound
 
 def parse(uri: str) -> AnyOutbound:
     parsed = urlsplit(uri)
-
-    if not parsed.scheme or not parsed.netloc:
-        raise InvalidURIError()
-
     query = parse_qs(parsed.query)
 
     match parsed.scheme.lower():
