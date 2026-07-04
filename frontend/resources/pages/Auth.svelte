0b78c88a71701ac6c82b00439f4c08f9f48fff12
awesfdawe <124027549+awesfdawe@users.noreply.github.com>
2026-07-04 19:09:20 +0200

    Revert "Svelte intertia litestar boilerplate"
    This reverts commit f80e3a6bb0c48a95a625e1dbbaa862d86728b4eb.

--- frontend/resources/pages/Auth.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import AuthForm from "$lib/components/features/AuthForm.svelte";
-</script>
-
-<main class="flex min-h-screen items-center justify-center">
-  <AuthForm />
-</main>
