59ca2e2e40c0a784bfea294bb4e9cf5487a738ea
awesfdawe <124027549+awesfdawe@users.noreply.github.com>
2026-08-08 04:27:51 +0200

    Add alembic and models

---
+++ backend/migrations/env.py
@@ -0,0 +1,56 @@
+from alembic import context
+from backend.logging import setup_logging
+from backend.models import Base
+from sqlalchemy import engine_from_config, pool
+
+setup_logging()
+
+from backend.config import get_config  # noqa: E402
+
+app_config = get_config()
+
+config = context.config
+
+config.set_main_option("sqlalchemy.url", f"sqlite:///{app_config.app.proxy_db_path}")
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        render_as_batch=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
---
+++ backend/src/backend/models.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+from sqlalchemy import JSON, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class ProxyProvider(Base):
+    __tablename__ = "proxy_providers"
+
+    name: Mapped[str] = mapped_column(primary_key=True)
+
+    proxies: Mapped[list["Proxy"]] = relationship(back_populates="provider", cascade="all, delete-orphan")
+
+
+class Proxy(Base):
+    __tablename__ = "proxies"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    outbound: Mapped[dict[str, Any]] = mapped_column(JSON, default=dict)
+
+    provider_name: Mapped[str] = mapped_column(ForeignKey("proxy_providers.name"))
+
+    provider: Mapped["ProxyProvider"] = relationship(back_populates="proxies")
